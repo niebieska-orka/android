082326012d967ba794299dac8c33ec724d15a927
pulnara <magnes.agnieszka@gmail.com>
2019-03-23 13:56:30 +0100

    Add some login work

---
+++ app/src/main/java/niebieskaorka/boradgames/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package niebieskaorka.boradgames.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import niebieskaorka.boradgames.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+    }
+}
---
+++ app/src/main/java/niebieskaorka/boradgames/ui/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package niebieskaorka.boradgames.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import niebieskaorka.boradgames.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_screen)
+    }
+}
